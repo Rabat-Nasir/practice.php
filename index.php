60eafb0a9d88ea19c16b0abc6bed5f4bbd6704c3
R/N <113181097+Rabat-Nasir@users.noreply.github.com>
2022-12-03 20:42:38 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Document</title>
+</head>
+<body>
+<?php
+// $x = 1.9e411;
+// var_dump($x);
+// echo"<br>";
+// $x - 5867;
+// var_dump(is_numeric($x));
+// echo"<br>";
+// $x = "hellow";
+// var_dump(is_numeric($x));
+// echo"<br>";
+// echo(min(0, 150, 30, 20, -8, -200));
+// echo"<br>";
+// echo(max(0, 150, 30, 20, -8, -200));
+// echo"<br>";
+// echo(abs(-9.7));
+// echo"<br>";
+// echo(round(8.60));
+// echo"<br>";
+// echo(round(0.49));
+// echo"<br>";
+// echo(rand(10, 100));
+// echo"<br>";
+// define("GREETING", "Welcome to W3Schools.com!");
+// echo GREETING;
+
+
+// echo"<br>";
+// $x = 5;
+// function test(){
+//   echo"<p>variable x inside function is : $x error</p>";
+//  }
+// //  test();
+// function subtract();{
+// $res - $num1.$num2
+// }
+// $x1 = 1024;
+// var_dump(is_int($x1));
+// echo"the given numberis $x1 is in integer </br>";
+// $x2 = 99.94;
+// var_dump(is_int($x2));
+// echo"the given numberis $x2 is not integer </br>";
+$x1 = 25;
+$y1 = 10;
+function mytest(){
+
+global $x1 , $x1;
+$x1 = $x1 + $x1;
+
+}
+mytest();
+echo $y1;
+?>
+</body>
+</html>
