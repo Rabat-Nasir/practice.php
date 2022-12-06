502646a939ec87f3410fd01b77a0ed42af2aeb93
R/N <113181097+Rabat-Nasir@users.noreply.github.com>
2022-12-06 21:02:49 +0500

    Add files via upload

---
+++ one.php
@@ -0,0 +1,87 @@
+// $x = 1.9e411;
+// var_dump($x);
+// echo"<br>";
+// $x - 5867;
+// var_dump(is_numeric($x));
+// echo"<br>";
+// $x = "hellow";
+// var_dump(is_numeric($x));
+// echo"<br>";
+// echo(min(0, 150, 30, 20, -8, -200));
+// echo"<br>";
+// echo(max(0, 150, 30, 20, -8, -200));
+// echo"<br>";
+// echo(abs(-9.7));
+// echo"<br>";
+// echo(round(8.60));
+// echo"<br>";
+// echo(round(0.49));
+// echo"<br>";
+// echo(rand(10, 100));
+// echo"<br>";
+// define("GREETING", "Welcome to W3Schools.com!");
+// echo GREETING;
+
+
+// echo"<br>";
+// $x = 5;
+// function test(){
+//   echo"<p>variable x inside function is : $x error</p>";
+//  }
+// //  test();
+// function subtract();{
+// $res - $num1.$num2
+// }
+// $x1 = 1024;
+// var_dump(is_int($x1));
+// echo"the given numberis $x1 is in integer </br>";
+// $x2 = 99.94;
+// var_dump(is_int($x2));
+// echo"the given numberis $x2 is not integer </br>";
+// $x1 = 25;
+// $y1 = 10;
+// function mytest(){
+
+// global $x1 , $x1;
+// $x1 = $x1 + $x1;
+
+// }
+// mytest();
+// // echo $y1;
+// $houroftheday = date("H");
+// if($houroftheday < "12"){
+//     echo "GOOD MORNING";
+// }
+// else{
+//     echo "Good Night";
+// }
+
+// $timeoftheday = date("H");
+//     if($timeoftheday < "18"){
+//         echo "Good Morning";
+//     }
+//     echo "it is evening yoo!";
+
+// $percentage = "75%";
+// if($percentage < "40%") {
+// echo "fail";
+// }
+// else if ($percentage <= "50%") {
+// echo "D-grade";
+// }
+//  else if ($percentage <= "60%") {
+// echo "C-grade";
+// }
+// else if ($percentage <= "70%") {
+//     echo "B-grade";
+// }
+//     else if ($percentage <= "90%") {
+//         echo "A-grade";
+//     }
+//         else if ($percentage <= "100%") {
+//             echo "A+grade";
+//         }
+//     else {
+//         echo"invalid-percentage";
+//     }
+   
